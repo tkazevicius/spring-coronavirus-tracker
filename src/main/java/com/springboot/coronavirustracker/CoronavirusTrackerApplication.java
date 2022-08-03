d60fd4fd1b2c0101b719e12c48430f5896c2d1f5
tkazevicius <79637177+tkazevicius@users.noreply.github.com>
2022-08-03 13:17:16 +0300

    Add files via upload

---
+++ src/main/java/com/springboot/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
